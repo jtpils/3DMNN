b1436bf0c353a0a4844ae6395530ec90f744483e
VikVelev <viktorvelev8@gmail.com>
2017-11-18 22:54:49 +0200

    Wrote simple viewport

---
+++ obj_import.py
@@ -0,0 +1,18 @@
+import pyglet
+import pywavefront as pyw
+from viewport import obj_viewport
+
+def update(dt):
+    windw.file_name = "model.obj"    
+    
+    windw.rotation += 45 * dt
+
+    if windw.rotation > 720:
+       windw.rotation = 0
+
+if __name__ == "__main__":
+    windw = obj_viewport(1280, 720, ".obj file viewer", resizable=True)
+    windw.mesh = pyw.Wavefront("model.obj")
+
+    pyglet.clock.schedule(update)
+    pyglet.app.run()
