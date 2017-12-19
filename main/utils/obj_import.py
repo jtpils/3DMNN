26c9121874d3b001f71f45504a2ba4dd1a858750
VikVelev <viktorvelev8@gmail.com>
2017-12-19 22:17:27 +0200

    Added shape generator. Proof of concept demo wip.

--- main/utils/obj_import.py
+++ main/utils/obj_import.py
@@ -14,18 +14,6 @@ if __name__ == "__main__":
     windw = obj_viewport(1280, 720, ".obj file viewer", resizable=True)
     windw.mesh = pyw.Wavefront("../model.obj")
     obj_defined = pyw.ObjParser(windw.mesh,"../model.obj")
-    
-    normals = []
-    
-    for i, vertex in enumerate(obj_defined.vertices, start=0):      
-        #calculate normals
-        #https://stackoverflow.com/questions/6656358/calculating-normals-in-a-triangle-mesh/6661242#6661242        
-        # x,y,z
-        vertex_normal = [.0, .0, .0]
-        
-        normals.append(vertex_normal)
-
-
 
     pyglet.clock.schedule(update)
     pyglet.app.run()
