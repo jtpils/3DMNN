c33299bc6a5388b3c8127cefee88f9f2104de31e
viktorv <viktorvelev8@gmail.com>
2017-12-25 18:49:15 +0200

    Added data generator

---
+++ main/utils/concept_data_generator.py
@@ -0,0 +1,13 @@
+import os
+import sys
+from shape_generator import cube_coords_generator as generator
+from shape_generator import cube_obj_wrapper as wrapper
+
+
+if __name__ == "__main__":
+    objects = 1000
+    if not sys.argv[1] == None: objects = int(sys.argv[1])
+    for i in range(0, objects):
+        file = open("./../../data/concept/cube" + str(i) + ".obj", "w")
+        file.write(wrapper(generator(symmetry=True)))
+        file.close()
