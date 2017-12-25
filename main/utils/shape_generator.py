86d654feedf49eb21273d8339d26ff248dea64ea
viktorv <viktorvelev8@gmail.com>
2017-12-25 17:23:42 +0200

    Working Cube Generator

--- main/utils/shape_generator.py
+++ main/utils/shape_generator.py
@@ -1,41 +1,57 @@
 import os
 import random as rand
 
-
 def cube_coords_generator(scale=1, symmetry=False):
     # [x, y, z]
     vertices = [1, 2, 3, 4, 5, 6, 7, 8]
-    lower_layer = vertices[0:4]
-    upper_layer = vertices[4:8]
+    lower_plane = vertices[0:4]
+    upper_plane = vertices[4:8]
 
     highest_point = rand.randint(0, 100)/100
-    lowest_point = round(highest_point - rand.randint(0, 100)/100, 4)
+    lowest_point = round(highest_point - rand.randint(30, 100)/100, 4)
 
-    for i in range(0, len(upper_layer)):
-        x = rand.randint(0, 100)/100
-        z = rand.randint(0.00, 100)/100
-        upper_layer[i] = [x, highest_point, z]
-        lower_layer[i] = [x, lowest_point, z]
+    z_1 = rand.randint(50, 100)/100
+    x_1 = rand.randint(0, 50)/100
+    x_2 = rand.randint(0, 50)/100
+    z_2 = rand.randint(50, 100)/100
+
+    first_point = { "x": x_1,"y": lower_plane, "z": z_1 }
+    second_point = { "x": x_2, "y": lower_plane, "z": z_1}
+
+    first_plane = [0,0,0,0]
+
+    first_plane[0] = first_point
+    first_plane[1] = second_point
+    first_plane[2] = { "x": x_1,"y": lower_plane, "z": z_2 }
+    first_plane[3] = { "x": x_2,"y": lower_plane, "z": z_2 }
+
+    for i in range(0,4):
+        upper_plane[i] = { "x": first_plane[i]["x"], "y": highest_point, "z": first_plane[i]["z"]}
+        lower_plane[3-i] = { "x": first_plane[i]["x"], "y": lowest_point, "z": first_plane[i]["z"]}
 
     if not symmetry:
-        for i in range(0, len(lower_layer)):
+        for i in range(0, len(lower_plane)):
             x = (rand.randint(0, 100)/100)*scale
             z = (rand.randint(0, 100)/100)*scale
+            lower_plane[i] = { "x": x_, "y": lowest_point, "z": z_}
 
-            lower_layer[i] = [x, lowest_point, z]
-
-    return lower_layer + upper_layer
+    print(lower_plane)
+    print(upper_plane)
+    return lower_plane + upper_plane
 
 def cube_obj_wrapper(coords, name="object"):
-    lines = ""
-    for i in range(0, len(coords)):
-        lines += "v " + str(coords[i][0]) + " " + str(coords[i][1]) + " " + str(coords[i][2]) + "\n"
 
-    lines += '''f 1 4 8 5\nf 4 3 7 8\nf 3 2 6 7\nf 2 1 5 6\nf 1 2 3 4\nf 5 6 7 8'''
+    lines = ""
+    
+    for i in range(0, len(coords)):
+        lines += "v " + str(coords[i]["x"]) + " " + str(coords[i]["y"]) + " " + str(coords[i]["z"]) + " #" + str(i + 1) + "\n"
+    # Пълно шано, е това свързване, едно по едно ги пробвах.
+    lines += 'f 2 1 3 4\nf 6 5 7 8\nf 1 2 7 8\nf 3 4 5 6\nf 2 4 5 7\nf 1 3 6 8'
+    print(lines)    
     file = open(name+".obj", "w")
     file.write(lines)
     file.close()
     return lines
 
 
-print(cube_obj_wrapper(cube_coords_generator(symmetry=True)))
+cube_obj_wrapper(cube_coords_generator(symmetry=True))
