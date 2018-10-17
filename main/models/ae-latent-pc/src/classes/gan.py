3cbd68dc2b625479e935024f9dfda6547318a85b
VikVelev <viktorvelev8@gmail.com>
2018-10-18 01:05:39 +0300

    Added some more base files, rewriting

---
+++ main/models/ae-latent-pc/src/classes/gan.py
@@ -0,0 +1,37 @@
+import os.path as osp
+import warnings
+import tensorflow as tf
+
+from utils.io.basics import create_dir
+
+from neural_network import NeuralNetwork
+
+
+class GAN(NeuralNetwork):
+
+    def __init__(self, name, graph):
+        NeuralNetwork.__init__(self, name, graph)
+
+    def save_model(self, tick):
+        self.saver.save(self.sess, self.MODEL_SAVER_ID, global_step=tick)
+
+    def restore_model(self, model_path, epoch, verbose=False):
+        '''Restore all the variables of a saved model.
+        '''
+        self.saver.restore(self.sess, osp.join(model_path, self.MODEL_SAVER_ID + '-' + str(int(epoch))))
+
+        if self.epoch.eval(session=self.sess) != epoch:
+            warnings.warn('Loaded model\'s epoch doesn\'t match the requested one.')
+        else:
+            if verbose:
+                print('Model restored in epoch {0}.'.format(epoch))
+
+    def optimizer(self, learning_rate, beta, loss, var_list):
+        initial_learning_rate = learning_rate
+        optimizer = tf.train.AdamOptimizer(initial_learning_rate, beta1=beta).minimize(loss, var_list=var_list)
+        return optimizer
+
+    def generate(self, n_samples, noise_params):
+        noise = self.generator_noise_distribution(n_samples, self.noise_dim, **noise_params)
+        feed_dict = {self.noise: noise}
+        return self.sess.run([self.generator_out], feed_dict=feed_dict)[0]
